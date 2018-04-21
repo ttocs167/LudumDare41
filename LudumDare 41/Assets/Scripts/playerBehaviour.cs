ba0eb3ce8dae1a2df2b08e78e3b59ef9c69fc6c8
ttocs167 <ttocs167@gmail.com>
2018-04-21 04:11:40 +0100

    Created basic player and tower scripts
    Player can move with arrow keys
    Tower is capable of shooting bullets towards player, they dont do
    anything yet.

---
+++ LudumDare 41/Assets/Scripts/playerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehaviour : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    public float maxSpeed = 10;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(input * moveSpeed);
+        }
+    }
+}
---
+++ LudumDare 41/Assets/Scripts/towerBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerBehaviour : MonoBehaviour
+{
+    public float range = 20;
+    public float damage;
+    public float rateOfFire;
+    public GameObject bulletType;
+    public float bulletSpeed;
+    public float rangeTime = 2f;
+
+    private GameObject target;
+    private float nextFire = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 heading = (target.transform.position - this.transform.position);
+        var distance = heading.magnitude;
+        Vector2 lookDirection = heading / distance;
+
+        if (heading.sqrMagnitude < range * range)
+        {
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + rateOfFire;
+
+                GameObject bullet = (GameObject)Instantiate(bulletType, transform.position, transform.rotation);
+                bullet.GetComponent<Rigidbody2D>().AddForce(lookDirection * bulletSpeed);
+
+                Destroy(bullet, rangeTime);
+            }
+        }
+    }
+}
